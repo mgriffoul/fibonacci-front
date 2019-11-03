696e775bd3fbaa11b019dcf7621f6be5de57d54f
mathieu_griffoul <mgriffoul@oui.sncf>
2019-11-03 22:50:58 +0100

    Starting Css

--- src/app/fibonacci-form/fibonacci-form.component.ts
+++ src/app/fibonacci-form/fibonacci-form.component.ts
@@ -7,7 +7,6 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./fibonacci-form.component.scss']
 })
 export class FibonacciFormComponent implements OnInit{
-
   registerForm: FormGroup;
   submitted = false;
 
@@ -15,15 +14,17 @@ export class FibonacciFormComponent implements OnInit{
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
-      requestNumber: ['', [Validators.required, Validators.pattern('^[0-9]$')]]
+      requestNumber: ['', [Validators.min(0),Validators.required, Validators.pattern('^[0-9]*$')]]
     });
   }
 
   onSubmit() {
     this.submitted = true;
     if (this.registerForm.invalid) {
+      alert('FAIL')
       return;
     }
+    alert('SUCCES')
     console.warn(this.registerForm.value);
   }
 
